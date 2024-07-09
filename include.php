7e0b865b56ee791230bfd85c404b04ec8c7ce8e7
VaghelaKaransinh <work.notlikekaran10@gmail.com>
2024-07-09 18:23:41 +0530

    Add files via upload
    Difference between Include And Required.

---
+++ include.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<head>
+    <title>Use of Require And Include</title>
+</head>
+<body>
+<?php
+require 'index.php';
+echo '<br/>hii<br/>';
+
+include 'index.php';
+echo '<br/>hii<br/>';
+?>
+</body>
+</html>
